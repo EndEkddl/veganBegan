956afbb3ba7264c9bc92cf52fb201d9d84afcd19
Inhye <sadnody@gmail.com>
2021-06-19 18:09:27 +0900

    Set table view contents with restaurants name

--- veganBegan/TableViewController.swift
+++ veganBegan/TableViewController.swift
@@ -15,29 +15,49 @@ class TableViewController: UITableViewController{
     
     override func viewDidLoad(){
         super.viewDidLoad()
-        for i in 0...44 {
+        
+     
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewItem")
+        DatabaseManager.sortbyDistance(latitude:37.551727, longitude: 126.941062, completion: {data in
+            for i in 0...44 {
+                let item1 = TableViewItem()
+                item1.text = data[i]["name"] as! String
+             //   print(item1.text)
+                self.items.append(item1)
+            }
+            self.tableView.reloadData()
+        })
+        /*for i in 0...44 {
             let item1 = TableViewItem()
             item1.text = name
-            items.append(item1)
-        }
+            self.items.append(item1)
+        }*/
     }
     
     // MARK:- Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 45;
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
     UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewItem", for: indexPath)
-        let item = items[indexPath.row]
 
-        configureText(for: cell, with: item)
-        return cell
-    }
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewItem", for: indexPath)
+            let item = items[indexPath.row]
+            cell.tag = 1000
+            configureText(for: cell, with: item)
+        
+            return cell
+        }
+
+        
+   
+    
     
     func configureText(for cell: UITableViewCell, with item: TableViewItem) {
-              let label = cell.viewWithTag(1000) as! UILabel
+       // let label = cell.viewW
+        let label = (cell.viewWithTag(1000) as! UITableViewCell).textLabel as! UILabel
               label.text = item.text
+            //    label.tag = 1000
     }
 }
